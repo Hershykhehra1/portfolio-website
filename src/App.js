a1b333f674261e8035c1bd8d30e448e091f59d8f
Harshaun <harshaunkhehra@yahoo.com>
2024-11-13 14:51:43 -0800

    minor formatting changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
@@ -7,6 +8,9 @@ import Skills from "./components/Skills";
 import SocialLinks from "./components/SocialLinks";
 
 function App() {
+  useEffect(() => {
+    document.title = "Harshaun Khehra's Portfolio";
+  }, []);
   return (
     <div>
       <NavBar />
